d261c803988142aaa82fd111ea00e2ad2bbefcd1
clackgot <clackgot@gmail.com>
2021-01-02 17:36:20 +0300

    Added console inputs

--- PageDownloaderSharp/Program.cs
+++ PageDownloaderSharp/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace PageDownloaderSharp
+﻿using System;
+
+namespace PageDownloaderSharp
 {
     partial class Program
     {
         static void Main(string[] args)
         {
-            SdoControl sdo = new SdoControl("clackgot@gmail.com", "uVJ3e3Uf");
+            string login, password, testUrl;
+            Console.Write("Логин:");
+            login = Console.ReadLine();
+            Console.Write("Пароль:");
+            password = Console.ReadLine();
+            Console.Write("Ссылка на тест:");
+            testUrl = Console.ReadLine();
+            SdoControl sdo = new SdoControl(login, password);
+            var testModel = new TestModel(sdo.GetPage(testUrl));
+            
+            Console.ReadKey();
         }
     }
 }
---
+++ PageDownloaderSharp/TestModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace PageDownloaderSharp
+{
+    class TestModel
+    {
+        private AngleSharp.Html.Dom.IHtmlDocument document;
+        public string Author { get; set; }
+        public string Mark { get; set; }
+        public TestModel(AngleSharp.Html.Dom.IHtmlDocument htmlDocument)
+        {
+            document = htmlDocument;
+
+            setAuthor();
+            setMark();
+            Print();
+        }
+
+        private void setAuthor()
+        {
+            Author = document.QuerySelector("div#user-picture div").TextContent;
+        }
+        private void setMark()
+        {
+            var element = document.All.First(o => o.TextContent == "Оценка" && o.TagName == "TH"); // If text, assign id.
+            if(element != null)
+            {
+                Mark = element.ParentElement.QuerySelector("td b").TextContent;
+            }
+            else
+            {
+                Console.WriteLine("Оценка не найдена");
+            }
+            
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(document.Title);
+            Console.WriteLine("Автор: " + Author);
+            Console.WriteLine("Оценка: " + Mark + "/100");
+        }
+    }
+}
